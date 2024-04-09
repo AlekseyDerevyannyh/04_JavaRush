39dc5eff8bddb49f8412d0bab7af412c866b5c6a
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-04-09 21:24:52 +0300

    add task1017

---
+++ src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Africa africa = new Africa(100);
+        Antarctica antarctica = new Antarctica(50);
+        Australia australia = new Australia(70);
+        Eurasia eurasia = new Eurasia(300);
+        NorthAmerica northAmerica = new NorthAmerica(200);
+        SouthAmerica southAmerica = new SouthAmerica(190);
+    }
+}
