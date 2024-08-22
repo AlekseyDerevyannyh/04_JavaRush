8c90278fa9efd2a9a72eac21cca30c3956a3d621
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-08-22 09:56:06 +0300

    add task1523

---
+++ src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    private Solution() {}
+
+    Solution(String s) {}
+
+    protected Solution(Integer n) {}
+
+    public Solution(Boolean b) {}
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(String s) {
+        super(s);
+    }
+
+    protected SubSolution(Integer n) {
+        super(n);
+    }
+
+    public SubSolution(Boolean b) {
+        super(b);
+    }
+}
