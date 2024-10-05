a0ffa0a1ce7ff366325a979bad790d9010d421ce
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-10-05 13:10:37 +0300

    add task1630

---
+++ src/com/javarush/task/task16/task1630/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task16.task1630;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Последовательный вывод файлов
+*/
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //напишите тут ваш код
+    static {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+        } catch (IOException e) {
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        f.join();
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    //напишите тут ваш код
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        private String fullFileName;
+        private String fileContent = "";
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fullFileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            return fileContent;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader reader = new BufferedReader(new FileReader(fullFileName)))  {
+                StringBuilder content = new StringBuilder();
+                while (reader.ready()) {
+                    content.append(reader.readLine());
+                    content.append(" ");
+                }
+                content.deleteCharAt(content.length() - 1);
+                fileContent = content.toString();
+            } catch(IOException e) {
+            }
+        }
+    }
+}
