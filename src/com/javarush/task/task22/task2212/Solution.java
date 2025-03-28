397664a896a912abe5807a4ed92e2eed5272b2a5
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-03-28 15:28:44 +0300

    update task2212

--- src/com/javarush/task/task22/task2212/Solution.java
+++ src/com/javarush/task/task22/task2212/Solution.java
@@ -8,41 +8,8 @@ public class Solution {
     public static boolean checkTelNumber(String telNumber) {
         if (telNumber == null)
             return false;
-        if (telNumber.matches("^\\+\\d{12}"))
-            return true;
-        if (telNumber.matches("^\\+\\d\\(\\d{3}\\)\\d{8}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{2}\\(\\d{3}\\)\\d{7}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{3}\\(\\d{3}\\)\\d{6}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{4}\\(\\d{3}\\)\\d{5}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{5}\\(\\d{3}\\)\\d{4}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{6}\\(\\d{3}\\)\\d{3}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{7}\\(\\d{3}\\)\\d{2}"))
-            return true;
-        if (telNumber.matches("^\\+\\d{8}\\(\\d{3}\\)\\d"))
-            return true;
-        if (telNumber.matches("^\\d\\d{9}"))
-            return true;
-        if (telNumber.matches("^\\(\\d{3}\\)\\d{7}"))
-            return true;
-        if (telNumber.matches("^\\d\\(\\d{3}\\)\\d{6}"))
-            return true;
-        if (telNumber.matches("^\\d{2}\\(\\d{3}\\)\\d{5}"))
-            return true;
-        if (telNumber.matches("^\\d{3}\\(\\d{3}\\)\\d{4}"))
-            return true;
-        if (telNumber.matches("^\\d{4}\\(\\d{3}\\)\\d{3}"))
-            return true;
-        if (telNumber.matches("^\\d{5}\\(\\d{3}\\)\\d{2}"))
-            return true;
-        if (telNumber.matches("^\\d{6}\\(\\d{3}\\)\\d"))
-            return true;
-        return false;
+        return (telNumber.matches("^\\+(\\d[()]?){12}$") || telNumber.matches("^([()]?\\d){10}$"))
+                && telNumber.matches("^(\\+)?(\\d+)?(\\(\\d{3}\\))?\\d+$");
     }
 
     public static void main(String[] args) {
