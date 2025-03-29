99323d3d8cf2e06e2fd00c053257a995940667db
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-03-29 11:53:20 +0300

    update task2312

--- src/com/javarush/task/task23/task2312/Room.java
+++ src/com/javarush/task/task23/task2312/Room.java
@@ -24,6 +24,16 @@ public class Room {
 
     public void print() {}
 
+    public void createMouse() {
+        int x = (int) (Math.random() * width);
+        int y = (int) (Math.random() * height);
+        mouse = new Mouse(x, y);
+    }
+
+    public void eatMouse() {
+        createMouse();
+    }
+
     public int getWidth() {
         return this.width;
     }
