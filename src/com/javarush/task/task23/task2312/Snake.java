9268dcde857cda81e0a740d3f6eed3074451b424
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-03-29 10:20:51 +0300

    update task2312

--- src/com/javarush/task/task23/task2312/Snake.java
+++ src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,25 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.List;
+
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public List<SnakeSection> getSections() {
+        return this.sections;
+    }
+
+    public boolean isAlive() {
+        return this.isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return this.direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
