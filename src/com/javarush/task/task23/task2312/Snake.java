21782e9f9befcb0f781af217da9e2cc752bfffcb
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-03-30 11:55:55 +0300

    update task2312

--- src/com/javarush/task/task23/task2312/Snake.java
+++ src/com/javarush/task/task23/task2312/Snake.java
@@ -21,9 +21,20 @@ public class Snake {
         else if (direction.equals(SnakeDirection.DOWN))     move(0, 1);
         else if (direction.equals(SnakeDirection.LEFT))     move(-1, 0);
     }
-
+    
     public void move(int x, int y) {
 
+    }
+
+    public void checkBorders(SnakeSection head) {
+        if (!((head.getX() >= 0 && head.getX() < Room.game.getWidth())
+                && (head.getY() >=0 && head.getY() < Room.game.getHeight())))
+            isAlive = false;
+    }
+
+    public void checkBody(SnakeSection head) {
+        if (sections.contains(head))
+            isAlive = false;
     }
 
     public int getX() {
