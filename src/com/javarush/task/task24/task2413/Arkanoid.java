659ac80d9c0f238e28c284cc0f2e74f8c4549f7b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-04-04 08:48:04 +0300

    update task2413

--- src/com/javarush/task/task24/task2413/Arkanoid.java
+++ src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     private int width;
     private int height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
 
     public Arkanoid(int width, int height) {
         this.width = width;
@@ -21,11 +26,35 @@ public class Arkanoid {
         return this.height;
     }
 
+    public Ball getBall() {
+        return this.ball;
+    }
+
+    public Stand getStand() {
+        return this.stand;
+    }
+
+    public List<Brick> getBricks() {
+        return this.bricks;
+    }
+
     public void setWidth(int width) {
         this.width = width;
     }
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 }
