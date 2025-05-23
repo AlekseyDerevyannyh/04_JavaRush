9153b3cbbf6db6e0088ff844654303a75ce4699f
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-05-23 12:49:19 +0300

    update task3608

--- src/com/javarush/task/task36/task3608/controller/Controller.java
+++ src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,17 +1,23 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void onShowAllUsers() {
         model.loadUsers();
         usersView.refresh(model.getModelData());
     }
 
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
 
     public void setModel(Model model) {
         this.model = model;
@@ -21,8 +27,7 @@ public class Controller {
         this.usersView = usersView;
     }
 
-    public void onShowAllDeletedUsers() {
-        model.loadDeletedUsers();
-        usersView.refresh(model.getModelData());
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 }
