f6149a4ddad22b5508ab96b9a271fb18012d2d5e
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-07-12 12:26:27 +0300

    update task2909

--- src/com/javarush/task/task29/task2909/user/User.java
+++ src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,8 @@ public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
+    private boolean man;
+    private Address address;
 
     private Work work;
 
@@ -28,6 +26,10 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+        return work.getBoss();
     }
 
     public String getName() {
@@ -55,23 +57,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -80,5 +82,13 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 }
