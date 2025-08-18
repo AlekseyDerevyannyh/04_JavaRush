ff3f75f2ffe332873c5a3db50c76a4327c862773
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-08-18 15:09:23 +0300

    update task3008

--- src/com/javarush/task/task30/task3008/client/Client.java
+++ src/com/javarush/task/task30/task3008/client/Client.java
@@ -79,11 +79,27 @@ public class Client {
         }
     }
 
-    public static class SocketThread extends Thread {
+    public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            // Выводим текст сообщения в консоль
+            ConsoleHelper.writeMessage(message);
+        }
 
-        @Override
-        public void run() {
+        protected void informAboutAddingNewUser(String userName) {
+            // Выводим информацию о добавлении участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' присоединился к чату.");
+        }
 
+        protected void informAboutDeletingNewUser(String userName) {
+            // Выводим информацию о выходе участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' покинул чат.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
         }
     }
 }
