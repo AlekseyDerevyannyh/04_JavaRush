48958f18424b1262134a11ae7a01fc5fc65e7ef7
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-10-11 12:08:50 +0300

    update task3110

--- src/com/javarush/task/task31/task3110/Archiver.java
+++ src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,23 +1,44 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
 
+    public static Operation askOperation() throws IOException {
+        String menuStr = "Выберете операцию:\n" +
+                "0 - упаковать файлы в архив\n" +
+                "1 - добавить файл в архив\n" +
+                "2 - удалить файл из архива\n" +
+                "3 - распаковать архив\n" +
+                "4 - просмотреть содержимое архива\n" +
+                "5 - выход";
+        ConsoleHelper.writeMessage(menuStr);
+        int command = ConsoleHelper.readInt();
+        for (Operation operation : Operation.values()) {
+            if (operation.ordinal() == command) {
+                return operation;
+            }
+        }
+        throw new IOException("Некорректный ввод!");
+    }
+
     public static void main(String[] args) throws Exception {
-        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Введите полный путь файла архива:");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
-
-        System.out.println("Введите полное имя файла для архивации:");
-        zipFileManager.createZip(Paths.get(bis.readLine()));
-
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        Operation operation = null;
+        while (operation != Operation.EXIT) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
     }
 }
