c0a97c1a82d5310a9da3ec20a43aa775616ee729
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-10-15 16:11:43 +0300

    update task3110

--- src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -7,10 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
+
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь файла архива: ");
-        String pathToZip = ConsoleHelper.readString();
-        Path path = Paths.get(pathToZip);
-        return new ZipFileManager(path);
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
