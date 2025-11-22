41d973f8a90a40d1ef8cc3669e46738847122df6
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-11-22 10:36:00 +0300

    update task3209

--- src/com/javarush/task/task32/task3209/Controller.java
+++ src/com/javarush/task/task32/task3209/Controller.java
@@ -64,4 +64,12 @@ public class Controller {
         }
         return  writer.toString();
     }
+
+    public void createNewDocument() {}
+
+    public void openDocument() {}
+
+    public void saveDocument() {}
+
+    public void saveDocumentAs() {}
 }
