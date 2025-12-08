320ba1d684e1961754642451244893dcf9b7d97d
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-08 10:54:44 +0300

    add task2913

---
+++ src/com/javarush/task/task29/task2913/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task29.task2913;
+
+import java.util.Random;
+
+/* 
+Замена рекурсии
+*/
+
+public class Solution {
+    private static int numberA;
+    private static int numberB;
+
+    public static String getAllNumbersBetween(int a, int b) {
+        if (a == b) {
+            return Integer.toString(a);
+        }
+        StringBuilder result = new StringBuilder();
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                result.append(i).append(" ");
+            }
+        } else {
+            for (int i = a; i >= b ; i--) {
+                result.append(i).append(" ");
+            }
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        numberA = random.nextInt(1000);
+        numberB = random.nextInt(1000);
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+}
