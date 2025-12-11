f4b732bd5cd85d927d680832575c65c6282bfe5e
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-11 13:11:00 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/Restaurant.java
+++ src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,11 +1,16 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 import java.io.IOException;
+import java.util.Observer;
 
 public class Restaurant {
 
     public static void main(String[] args) throws IOException {
         Tablet tablet = new Tablet(5);
+        Cook cook = new Cook("Amigo");
+        tablet.addObserver(cook);
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
