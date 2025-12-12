cc4bba29a8134995b44d752bcfcfec0ab73de00a
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-12 10:24:48 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/kitchen/Cook.java
+++ src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -3,7 +3,7 @@ package com.javarush.task.task27.task2712.kitchen;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Cook implements Observer {
+public class Cook extends Observable implements Observer {
     private String name;
 
     public Cook(String name) {
@@ -11,8 +11,10 @@ public class Cook implements Observer {
     }
 
     @Override
-    public void update(Observable o, Object arg) {
-        System.out.println("Start cooking - " + arg);
+    public void update(Observable tablet, Object order) {
+        System.out.println("Start cooking - " + order);
+        setChanged();
+        notifyObservers(order);
     }
 
     @Override
