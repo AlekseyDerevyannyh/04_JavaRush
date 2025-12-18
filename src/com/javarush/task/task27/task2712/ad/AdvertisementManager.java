8a71a4ab21954159c1429edb6fb37cfcd9f7b9c6
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-18 20:20:36 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -12,7 +15,13 @@ public class AdvertisementManager {
         if (storage.list() == null || storage.list().isEmpty()) {
             throw new NoVideoAvailableException();
         }
+        List<Advertisement> advertisments = storage.list();
+        List<Advertisement> result = new ArrayList<>();
+
+
 
 
     }
+
+
 }
