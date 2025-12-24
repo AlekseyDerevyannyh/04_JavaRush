71d75422e92eca5e5a542857071ceecceb007e42
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-24 14:48:30 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,10 +1,29 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
     private static class InstanceHolder {
         private static final StatisticManager instance = new StatisticManager();
+    }
+
+    private static class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage;
+
+        public StatisticStorage() {
+            storage = new HashMap<>();
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<>());
+            }
+        }
     }
 
     private StatisticManager() {}
