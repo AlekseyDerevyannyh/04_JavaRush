d7eee154e262d9d5eace83742ac8bddc9d810558
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-12-26 17:03:15 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,15 +1,14 @@
 package com.javarush.task.task27.task2712.statistic;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StatisticManager {
     private StatisticStorage statisticStorage = new StatisticStorage();
+    private Set<Cook> cooks = new HashSet<>();
 
     private static class InstanceHolder {
         private static final StatisticManager instance = new StatisticManager();
@@ -38,5 +37,9 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
         statisticStorage.put(data);
+    }
+
+    public void register(Cook cook) {
+        cooks.add(cook);
     }
 }
