72371d945b2ac22ca2b2fde581c40bc51ebc9f56
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-03-25 15:59:57 +0300

    update task2712

--- src/com/javarush/task/task27/task2712/Restaurant.java
+++ src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
-import com.javarush.task.task27.task2712.kitchen.Waiter;
 import com.javarush.task.task27.task2712.statistic.StatisticManager;
 
 import java.util.ArrayList;
@@ -11,30 +10,34 @@ public class Restaurant {
     private static final int ORDER_CREATING_INTERVAL = 100;
 
     public static void main(String[] args) {
-        Cook cook = new Cook("Amigo");
-        Cook cook2 = new Cook("Diego");
-        StatisticManager.getInstance().register(cook);
-        StatisticManager.getInstance().register(cook2);
         List<Tablet> tablets = new ArrayList<>();
-
-        Waiter waiter = new Waiter();
-        cook.addObserver(waiter);
-        cook2.addObserver(waiter);
-
         for (int i = 0; i < 5; i++) {
             tablets.add(new Tablet(i + 1));
-            tablets.get(i).addObserver(cook);
-            tablets.get(i).addObserver(cook2);
+        }
+
+        Cook cook1 = new Cook("Amigo");
+        Cook cook2 = new Cook("Diego");
+
+        StatisticManager.getInstance().register(cook1);
+        StatisticManager.getInstance().register(cook2);
+
+        OrderManager orderManager = new OrderManager();
+
+        for (Tablet tablet :
+                tablets) {
+            tablet.addObserver(orderManager);
         }
 
         Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
         thread.start();
+
         try {
             Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            return;
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException ignore) {
         }
-        thread.interrupt();
 
         DirectorTablet directorTablet = new DirectorTablet();
         directorTablet.printAdvertisementProfit();
