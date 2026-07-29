6733c75e83bea86c722e1e14fcba153a6273c45b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-07-29 18:01:35 +0300

    add task3102

--- src/com/javarush/task/task31/task3101/Solution.java
+++ src/com/javarush/task/task31/task3101/Solution.java
@@ -5,58 +5,62 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
 
-/* 
+/*
 Проход по дереву файлов
 */
 
 public class Solution {
-    static void main(String[] args) throws IOException {
-        if (args == null || args.length < 2) {
-            System.out.println("Input args error!");
-            return;
-        }
+    public static void main(String[] args) {
         String path = args[0];
         String resultFileAbsolutePath = args[1];
+        try {
+            File resultFile = new File(resultFileAbsolutePath);
+            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt");
+            if (FileUtils.isExist(dest)) {
+                FileUtils.deleteFile(dest);
+            }
+            FileUtils.renameFile(resultFile, dest);
 
-        File sourceFile = new File(resultFileAbsolutePath);
-        File destinationFile = new File(sourceFile.getParent() + "/allFilesContent.txt");
-        FileUtils.renameFile(sourceFile, destinationFile);
-
-        File sourceDir = new File(path);
-
-        Set<File> files = new HashSet<>();
-        getAllTxtFilesInSubfolders(sourceDir, files);
-        if (files.isEmpty()) {
-            return;
-        }
-
-        try (FileOutputStream fileOutputStream = new FileOutputStream(destinationFile, true)) {
-            for (File file : files) {
-                if (!file.getAbsolutePath().equals(destinationFile.getAbsolutePath())) {
-                    byte[] bytes = Files.readAllBytes(file.toPath());
-                    for (byte inputByte : bytes) {
-                        fileOutputStream.write(inputByte);
-                    }
+            Map<String, byte[]> fileTree = getFileTree(path);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(dest)) {
+                for (byte[] bytes : fileTree.values()) {
+                    fileOutputStream.write(bytes);
                     fileOutputStream.write("\n".getBytes());
                 }
             }
+        } catch (IOException ignored) {
         }
     }
 
-    private static void getAllTxtFilesInSubfolders(File sourceDir, Set<File> files) {
-        File[] currentFiles = sourceDir.listFiles();
-        if (currentFiles == null) {
-            return;
+    public static Map<String, byte[]> getFileTree(String root) throws IOException {
+        Map<String, byte[]> result = new TreeMap<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private Map<String, byte[]> result;
+
+        public GetFiles(Map<String, byte[]> result) {
+            this.result = result;
         }
-        for (File file : currentFiles) {
-            if (file.isDirectory()) {
-                getAllTxtFilesInSubfolders(file, files);
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                if (file.length() <= 50) {
+                    result.put(path.getFileName().toString(), Files.readAllBytes(path));
+                }
             }
-            if (file.isFile() && file.getName().endsWith(".txt") && file.length() <= 50L) {
-                files.add(file);
-            }
+            return super.visitFile(path, basicFileAttributes);
         }
     }
 }
---
+++ src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+/* 
+Находим все файлы
+*/
+
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        List<String> result;
+
+        public GetFiles(List<String> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                result.add(file.getAbsolutePath());
+            }
+            return super.visitFile(path, attrs);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
