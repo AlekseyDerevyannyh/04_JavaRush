2c566461f08cb4f33df07dc22b45db96284f3d1c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-09-22 22:12:28 +0300

    Docker add task 14

---
+++ src/ru/javarush/system/linux/level01/task14/solution.sh
@@ -0,0 +1,19 @@
+# Создание файла example2.txt и заполнение его 10 строками текста
+echo "line1" > example2.txt
+echo "line2" >> example2.txt
+echo "line3" >> example2.txt
+echo "line4" >> example2.txt
+echo "line5" >> example2.txt
+echo "line6" >> example2.txt
+echo "line7" >> example2.txt
+echo "line8" >> example2.txt
+echo "line9" >> example2.txt
+echo "line10" >> example2.txt
+
+# Открытие файла с помощью less
+less example2.txt
+
+# В less:
+# - Нажмите 'Space' для перехода на следующую страницу.
+# - Нажмите 'b' для возврата на предыдущую страницу.
+# - Нажмите 'q' для выхода из программы.
