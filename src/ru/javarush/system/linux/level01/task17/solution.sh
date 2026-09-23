0540c23b3becf3351bb0dad4f220d5f4d545eb46
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-09-23 12:55:27 +0300

    Docker add task 17

---
+++ src/ru/javarush/system/linux/level01/task17/solution.sh
@@ -0,0 +1,18 @@
+# Создаем директорию task1 в домашней директории
+mkdir -p ~/task1
+
+# Переходим в созданную директорию
+cd ~/task1
+
+# Создаем пустой файл testfile.txt
+touch testfile.txt
+
+# Добавляем текст "Hello, Linux World!" в файл с помощью редактора nano
+nano testfile.txt
+# Напишите `Hello, Linux World!`
+# Нажмите `CTRL+O`
+# Нажмите `Enter`
+# Нажмите `CTRL+X`
+
+# Выводим содержимое файла testfile.txt на экран
+cat testfile.txt
