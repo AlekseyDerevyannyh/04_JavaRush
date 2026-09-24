3531bcdf7004427368959270e484502c370c1e74
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-09-24 11:54:09 +0300

    Docker add level 3 task 1

---
+++ src/ru/javarush/system/linux/level03/task01/solution.sh
@@ -0,0 +1,14 @@
+dpkg -s cron
+
+# Устанавливаем пакет cron
+sudo apt-get update
+sudo apt-get install -y cron
+
+# Запускаем службу cron
+sudo systemctl start cron
+
+# Останавливаем службу cron
+sudo systemctl stop cron
+
+# Проверяем статус службы cron
+sudo systemctl status cron
