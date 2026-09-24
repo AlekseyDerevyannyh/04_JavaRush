2faedf4dd6118c10cdce4bf9cfdf1984765c5c6b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2026-09-24 20:50:51 +0300

    Docker add level 3 task 17

---
+++ src/ru/javarush/system/linux/level03/task17/solution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Сохраняем имя файла из аргумента
+file_name="$1"
+
+# Проверка существования файла
+if [ -f $file_name ]; then
+    echo "Файл $file_name существует."
+else
+    echo "Файл $file_name не найден."
+fi
